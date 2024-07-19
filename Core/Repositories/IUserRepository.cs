d5edef3a97f0a0e45dfad4924423d859c4fa91cd
Cengizhan1 <cengizhany.cy@gmail.com>
2024-07-19 15:06:21 +0300

    IUserRepository and UserRepository added

---
+++ Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Dtos;
+using Core.Entities;
+
+namespace Core.Repositories
+{
+    public interface IUserRepository:IGenericRepository<User>
+    {
+        Task<IEnumerable<User>> GetActiveUser();
+        Task<IEnumerable<User>> GetUserGetUsersByCreatedDateBetween(DateTime startDate, DateTime endDate);
+    }
+}
---
+++ Repository/Repositories/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Repositories;
+
+namespace Repository.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+        public Task<IEnumerable<User>> GetActiveUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUserGetUsersByCreatedDateBetween(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
