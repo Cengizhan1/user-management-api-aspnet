b7986b75668e6d5170e8d59128ddc52eb9795d67
Cengizhan1 <cengizhany.cy@gmail.com>
2024-07-22 17:46:34 +0300

    program cs updated some scoped added

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,15 @@
+using System.Reflection;
+using API.Modules;
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+using Service.Mapping;
+using Core.UnitOfWorks;
+using Repository.UnitOfWorks;
+using Core.Repositories;
+using Repository.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +18,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAutoMapper(typeof(MapProfile));
+
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+    });
+});
+
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModule()));
+
+
+
 
 var app = builder.Build();
 
