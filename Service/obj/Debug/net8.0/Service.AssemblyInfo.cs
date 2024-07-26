8795aff7658175242ae0f248e3c4ba35e25b7f5c
Cengizhan1 <cengizhany.cy@gmail.com>
2024-07-26 11:02:33 +0300

    remove bin and obj folders

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5bd0e0ade8361a5c6bc2c44b94c0af2dba6bd74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d695787648f788b6fedaec0b698c5158a3090764")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
