2c9099084b7990fbeebc0eaf8c8dbb8f128157a8
Cengizhan1 <cengizhany.cy@gmail.com>
2024-08-07 12:27:29 +0300

    CustomBaseController added

---
+++ API/Controllers/CustomBaseController.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using Core.Dtos.BaseControllerResponse;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        public CustomBaseController()
+        {
+
+        }
+
+        protected virtual IActionResult ApiResponse()
+        {
+            return Ok(new SuccessDataResponse());
+        }
+
+        protected virtual IActionResult ApiResponse(HttpStatusCode httpStatusCode)
+        {
+            return SendResponse(new SuccessDataResponse(httpStatusCode));
+        }
+        protected virtual IActionResult ApiResponse<T>(CustomDataResponse<T> result)
+        {
+            return SendResponse(result);
+        }
+
+        protected virtual IActionResult ApiResponse(CustomApiResponse result)
+        {
+            return SendResponse(result);
+        }
+
+        protected virtual FileStreamResult ApiResponse(byte[] result, string contentType)
+        {
+            return new FileStreamResult(new MemoryStream(result), contentType);
+        }
+
+        private IActionResult SendResponse(dynamic result)
+        {
+            if (result.IsSuccessful)
+            {
+                switch (result.HttpStatusCode)
+                {
+                    case (int)HttpStatusCode.OK:
+                        return Ok(result);
+                    case (int)HttpStatusCode.Created:
+                        return Created("", result);
+                    case (int)HttpStatusCode.Accepted:
+                        return Accepted(result);
+                    case (int)HttpStatusCode.NoContent:
+                        return NoContent();
+                }
+            }
+            else
+            {
+                switch (result.HttpStatusCode)
+                {
+                    case (int)HttpStatusCode.BadRequest:
+                        return BadRequest(new ErrorDataResponse(result.ErrorMessageList, HttpStatusCode.BadRequest));
+                    case (int)HttpStatusCode.Unauthorized:
+                        return Unauthorized(new { Message = "Unauthorized request" });
+                    case (int)HttpStatusCode.Forbidden:
+                        return Forbid();
+                    case (int)HttpStatusCode.NotFound:
+                        return NotFound();
+                    default:
+                        return BadRequest();
+                }
+            }
+
+            return BadRequest();
+        }
+    }
+}
